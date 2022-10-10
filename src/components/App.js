6818714c282e2f230a62845f51cc49459c8c29d0
Dipanshu Raj <108553225+dpsdmh@users.noreply.github.com>
2022-10-10 18:24:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, {Component} from "react";
+import '../styles/App.css';
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+		super(props);
+		this.state={display:false};
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+handleClick(){
+ this.setState({display:true});
+
+ 
 }
+
+    render() {
+    	return(
+    		<div id="main">
+				<button id='click' onClick={this.handleClick} >Click</button>
+{this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:''}
+				{ /* Do not remove this main div!! */ }
+    		</div>
+    	);
+    }
+}
+
 
 
 export default App;
